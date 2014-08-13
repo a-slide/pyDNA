391f475db4bbd029766a2b4e9c9c4aa32a391c30
a-slide <adrien.leger@gmail.com>
2014-08-13 19:16:40 +0200

    Minor update of fill_between graph

--- src/Utilities.py
+++ src/Utilities.py
@@ -416,7 +416,8 @@ def count_seq (filename, seq_type="fasta"):
 
 #~~~~~~~GRAPHICAL UTILIES~~~~~~~#
 
-def fill_between_graph (X, Y, basename="out", img_type="png", title=None, xlabel=None, ylabel=None, baseline=0):
+def fill_between_graph (X, Y, basename="out", img_type="png", title=None, xlabel=None, ylabel=None,
+    baseline=0, xsize=15, ysize=10, dpi=100, fill_color='green'):
     """
     Trace a generic fill between graph with matplotlib pyplot
     @param X List of values for x axis
@@ -426,13 +427,18 @@ def fill_between_graph (X, Y, basename="out", img_type="png", title=None, xlabel
     @param ylabel Label for y axis (facultative)
     @param basename Output basename of the image file (Default "out")
     @param img_type Type of the image file (Default "png")
+    @param baseline lower value of the colorated area (Default 0)
+    @param xsize Width of the graphics (Default 15)
+    @param ysize Heigth of the graphics (Default 10)
+    @param dpi Resolution of the graphics (Default 100)
+    @param fill_color Color of the filled area (Default 'green')
     """
 
     # Require the Third party package matplotlib
     from matplotlib import pyplot as plt
 
     # Create a figure object and adding details
-    fig = plt.figure(figsize=(15, 10), dpi=100)
+    fig = plt.figure(figsize=(xsize, ysize), dpi=dpi)
     if title:
         plt.title(title)
     if xlabel:
@@ -441,7 +447,7 @@ def fill_between_graph (X, Y, basename="out", img_type="png", title=None, xlabel
         plt.ylabel(ylabel)
 
     # Plot an area representing the coverage depth
-    plt.fill_between(X, Y, baseline, facecolor='green', alpha=0.5)
+    plt.fill_between(X, Y, baseline, facecolor=fill_color, alpha=0.5)
 
     # Tweak spacing to prevent clipping of ylabel
     plt.subplots_adjust(left=0.15)
