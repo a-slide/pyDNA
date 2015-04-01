6643cb03c17ac49fe3ef3492770aa8b010364065
Adrien Leger <adrien.leger@inserm.fr>
2015-04-01 15:45:51 +0200

    Add a class to unzip files more efficiently than gzip

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ For now it includes BWA, Blast and Smith–Waterman wrappers as well as higher l
 * FastqFT : Filter fastq file based on quality and adapter trimming
 * RefMasker : Align a fasta reference against several fastq queries and mask homologies in the reference file
 * pySamTools :  Manipulate aligned reads manipulation though pysam
-* Utilities : Library of simple generic functions to manipulate paths and file, interact with command line interpreter and so on 
+* Utilities : Library of simple generic functions to manipulate paths and file, interact with command line interpreter and so on
 
 @copyright  [GNU General Public License v2](http://www.gnu.org/licenses/gpl-2.0.html)
 @author     Adrien Leger - 2014
@@ -18,5 +18,5 @@ For now it includes BWA, Blast and Smith–Waterman wrappers as well as higher l
 * [Atlantic Gene Therapies - INSERM 1089] (http://www.atlantic-gene-therapies.fr/)
 """
 
-__all__ = ["Utilities", "Blast", "Bwa", "FastqFT", "pySamTools", "Ssw", "RefMasker"]
+__all__ = ["Utilities", "Blast", "Bwa", "FastqFT", "pySamTools", "Ssw", "RefMasker", "Ungzip"]
 __version__ = 0.1
